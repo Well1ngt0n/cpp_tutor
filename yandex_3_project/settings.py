efc5bc1dc0d36dec187cf58b1eefb89c9e721306
Well1ngt0n <poyarkov12390@gmail.com>
2021-04-24 16:08:17 +0300

    and_one_more_commit

--- yandex_3_project/settings.py
+++ yandex_3_project/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = "D:\yandex_3_project\cpp_tutor\static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, "static")
