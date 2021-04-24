c492c6a20dae2070fc355c5235a6837cee70173a
Well1ngt0n <poyarkov12390@gmail.com>
2021-04-24 14:21:14 +0300

    Diploy-3

--- yandex_3_project/settings.py
+++ yandex_3_project/settings.py
@@ -119,9 +119,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = "D:\yandex_3_project\cpp_tutor\static"
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, "static")
 ]
